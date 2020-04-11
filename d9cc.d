651edfa9b33699e18d6347c6a769cef03f8865e2
private-yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-04-11 13:42:49 +0900

    Implement a recursive descendent parser.

--- d9cc.d
+++ d9cc.d
@@ -4,19 +4,21 @@ import std.uni : isSpace;
 import std.ascii : isDigit;
 import std.array : join;
 
+// Tokenizer
+
 enum TokenType
 {
     NUM, // Number literal
     ADD = '+',
     SUB = '-',
-    EOF // end literal
+    EOF // End marker
 }
 
 struct Token
 {
-    TokenType type;
-    int val;
-    string input;
+    TokenType type; // Token type
+    int val; // Number literal
+    string input; // Token string (for error reporting)
 }
 
 class QuitException : Exception
@@ -28,6 +30,12 @@ class QuitException : Exception
         super(null, file, line);
         this.return_code = return_code;
     }
+}
+
+void fail(Token t)
+{
+    stderr.writefln("Unexpected token: %s", t.input);
+    throw new QuitException(-1);
 }
 
 Token[] tokenize(string s)
@@ -74,10 +82,97 @@ Token[] tokenize(string s)
     return res;
 }
 
-void fail(Token t)
+enum NodeType
 {
-    stderr.writefln("Unexpected token: %s", t.input);
+    NUM, // Number literal
+    ADD = '+',
+    SUB = '-'
+}
+
+struct Node
+{
+    NodeType type; // Node type
+    Node* lhs = null;
+    Node* rhs = null;
+    int val; // Number literal
+}
+
+Node* new_node(NodeType op, Node* lhs, Node* rhs)
+{
+    Node* node = new Node;
+    node.type = op;
+    node.lhs = lhs;
+    node.rhs = rhs;
+    return node;
+}
+
+Node* new_node_num(int val)
+{
+    Node* node = new Node;
+    node.type = NodeType.NUM;
+    node.val = val;
+    return node;
+}
+
+Node* number(Token[] tokens, ref size_t pos)
+{
+    if (tokens[pos].type == TokenType.NUM)
+        return new_node_num(tokens[pos++].val);
+    stderr.writefln("Number expected, but got %s", tokens[pos].input);
     throw new QuitException(-1);
+}
+
+Node* expr(Token[] tokens)
+{
+    size_t pos;
+    Node* lhs = number(tokens, pos);
+    while (true)
+    {
+        TokenType op = tokens[pos].type;
+        if (op != '+' && op != '-')
+            break;
+        pos++;
+        lhs = new_node(cast(NodeType) op, lhs, number(tokens, pos));
+    }
+
+    if (tokens[pos].type != TokenType.EOF)
+        stderr.writefln("stray token: %s", tokens[pos].input);
+    return lhs;
+}
+
+// Code Generator
+
+string[] regs = ["rdi", "rsi", "r10", "r11", "r12", "r13", "r14", "r15"];
+
+string gen(Node* node, ref size_t cur)
+{
+    if (node.type == NodeType.NUM)
+    {
+        if (cur + 1 > regs.length)
+        {
+            stderr.writeln("register exhausted");
+            throw new QuitException(-1);
+        }
+        string reg = regs[cur++];
+        writefln("  mov %s, %d", reg, node.val);
+        return reg;
+    }
+
+    string dst = gen(node.lhs, cur);
+    string src = gen(node.rhs, cur);
+
+    switch (node.type)
+    {
+    case NodeType.ADD:
+        writefln("  add %s, %s", dst, src);
+        return dst;
+    case NodeType.SUB:
+        writefln("  sub %s, %s", dst, src);
+        return dst;
+    default:
+        stderr.writeln("unknown operator");
+        throw new QuitException(-1);
+    }
 }
 
 int main(string[] args)
@@ -90,39 +185,15 @@ int main(string[] args)
 
     try
     {
+        size_t cur;
         auto tokens = tokenize(args[1]);
+        Node* node = expr(tokens);
         writeln(".intel_syntax noprefix");
         writeln(".global main");
         writeln("main:");
 
-        if (tokens[0].type != TokenType.NUM)
-            fail(tokens[0]);
-        writefln("  mov rax, %d", tokens[0].val);
-
-        size_t i = 1;
-        while (tokens[i].type != TokenType.EOF)
-        {
-            if (tokens[i].type == '+')
-            {
-                i++;
-                if (tokens[i].type != TokenType.NUM)
-                    fail(tokens[i]);
-                writefln("  add rax, %d", tokens[i].val);
-                i++;
-                continue;
-            }
-            if (tokens[i].type == '-')
-            {
-                i++;
-                if (tokens[i].type != TokenType.NUM)
-                    fail(tokens[i]);
-                writefln("  sub rax, %d", tokens[i].val);
-                i++;
-                continue;
-            }
-
-            fail(tokens[i]);
-        }
+        // generate code while descending the parse tree.
+        writefln("  mov rax, %s", gen(node, cur));
         writeln("  ret");
     }
     catch (QuitException e)
